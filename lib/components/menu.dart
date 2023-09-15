d59661ddcad5893179384ae5ed3325152ac69ec9
Matheus Schuch <102114294+MatheusShu1@users.noreply.github.com>
2023-09-15 19:55:26 -0300

    .

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -1,57 +1,56 @@
-
 import 'package:flutter/material.dart';
 import 'package:precifica_ai/cadastro_produtos.dart';
 
 class PrincipalMenu {
-  static Drawer get(context){
-  return Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('Chana da Silva Sauro'),
-              accountEmail: Text('ricardo@gay.com'),
-              currentAccountPicture: CircleAvatar(
-                radius: 30,
-                backgroundImage: AssetImage('images/user.png'),
-              ),
+  static Drawer get(context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Chana da Silva Sauro'),
+            accountEmail: Text('ricardo@gay.com'),
+            currentAccountPicture: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage('images/user.png'),
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Página inicial'),
-              subtitle: Text('Início'),
-              trailing: Icon(Icons.new_label),
-            ),
-            ListTile(
-                leading: Icon(Icons.list),
-                title: Text('Produtos'),
-                subtitle: Text('Todos os produtos'),
-                trailing: Icon(Icons.shopping_bag),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => cadastroTrf()),
-                  );
-                }),
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text('Favoritos'),
-              subtitle: Text('Meus favoritos'),
-              trailing: Icon(Icons.arrow_forward),
-            ),
-            ListTile(
-              leading: Icon(Icons.task),
-              title: Text('Visualize suas tarefas...'),
-              subtitle: Text('Tasks'),
-              trailing: Icon(Icons.burst_mode),
-            ),
-            ListTile(
-              leading: Icon(Icons.task_sharp),
-              title: Text('Adicionar Novas Tarefas...'),
-              subtitle: Text('+ Tasks'),
-              trailing: Icon(Icons.task_alt),
-            )
-          ],
-        ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Página inicial'),
+            subtitle: Text('Início'),
+            trailing: Icon(Icons.new_label),
+          ),
+          ListTile(
+              leading: Icon(Icons.list),
+              title: Text('Produtos'),
+              subtitle: Text('Todos os produtos'),
+              trailing: Icon(Icons.shopping_bag),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => cadastroTrf()),
+                );
+              }),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Favoritos'),
+            subtitle: Text('Meus favoritos'),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            leading: Icon(Icons.task),
+            title: Text('Visualize suas tarefas...'),
+            subtitle: Text('Tasks'),
+            trailing: Icon(Icons.burst_mode),
+          ),
+          ListTile(
+            leading: Icon(Icons.task_sharp),
+            title: Text('Adicionar Novas Tarefas...'),
+            subtitle: Text('+ Tasks'),
+            trailing: Icon(Icons.task_alt),
+          )
+        ],
       ),
-}
+    );
+  }
 }
